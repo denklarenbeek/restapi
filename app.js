6e64aaf7bb4ffea7f3f69fdbfbd3a2b036aa66cb
Dennis Klarenbeek <dennisklarenbeek@MacBook-Pro-van-Dennis.local>
2016-10-28 15:04:53 +0200

    changed http put method

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(corser.create({
 }));
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With,Authorization,Access-Control-Allow-Origin');
-    res.header('Access-Control-Allow-Methods', 'POST,GET,DELETE');
+    res.header('Access-Control-Allow-Methods', 'POST,GET,DELETE,PUT');
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
